5c23f9beb8f7e008fa799090c954810d201a5678
孙一洋 <sunyiyang@sunyiyangdeMacBook-Air.local>
2026-09-11 19:16:20 +0800

    fix(miniapp): keep page configs build compatible

--- 08-微信小程序端/src/subpackages/scan/archive/index.config.ts
+++ 08-微信小程序端/src/subpackages/scan/archive/index.config.ts
@@ -1,3 +1,3 @@
-export default definePageConfig({
+export default {
   navigationBarTitleText: '健康档案',
-});
+};
--- 08-微信小程序端/src/subpackages/scan/scales/index.config.ts
+++ 08-微信小程序端/src/subpackages/scan/scales/index.config.ts
@@ -1,3 +1,3 @@
-export default definePageConfig({
+export default {
   navigationBarTitleText: '量表记录',
-});
+};
